6f91e003858e75de59435d46a3475cc00d1931b9
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-05-13 14:21:28 -0400

    Change values of string variables and add console.logs

--- variables.js
+++ variables.js
@@ -2,6 +2,10 @@ var firstName = "Ben"
 var petName = "Teddy"
 var lastName = "Rosner"
 
+firstName = "Patrick"
+petName = "Rock"
+lastName = "Star"
+
 var age = 31
 var escapeRooms = 143
 var statesTraveled = 7
@@ -9,3 +13,8 @@ var statesTraveled = 7
 var isHappy = true
 var isCoding = true
 var isTuringGrad = false
+
+console.log(firstName)
+console.log(escapeRooms)
+console.log(isCoding)
+console.log(isTuringGrad)
